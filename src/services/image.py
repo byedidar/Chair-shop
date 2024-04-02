b4ab23b2066fea3b9281678ba5fc3b497cd8a663
Didar Bauyrzhan <didar_lucifer@mail.ru>
2024-04-02 12:51:43 +0500

    feat: added services

---
+++ src/services/image.py
@@ -0,0 +1,4 @@
+from src.services.base import BaseService
+
+class ImageService(BaseService):
+    pass
---
+++ src/services/review.py
@@ -0,0 +1,4 @@
+from src.services.base import BaseService
+
+class ReviewService(BaseService):
+    pass
